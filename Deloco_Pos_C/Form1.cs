0494edd60e8d03615b7f3fec692f23274e79d435
Rudi Cluderay <cluderayd@gmail.com>
2017-09-17 13:10:31 +0100

    first commit

--- Deloco_Pos_C/Form1.cs
+++ Deloco_Pos_C/Form1.cs
@@ -7,7 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+ 
+
+using System.Net;
 namespace Deloco_Pos_C
 {
     public partial class Form1 : Form
@@ -16,5 +26,29 @@ namespace Deloco_Pos_C
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string func = txtFunction.Text;
+            string val = txtValue.Text;
+            string url = "https://00t1ug5s49.execute-api.eu-west-1.amazonaws.com/prod/apex-deepretail_deppcore?func=" + func + "&val=" + val;
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage responce =  client.GetAsync(url).Result)
+                {
+                    using (HttpContent content = responce.Content)
+                    {
+                        string mycontent =  content.ReadAsStringAsync().Result;
+                        txtResult.Text = mycontent;
+                        string newstring=mycontent.Replace("\"", "'");
+
+                        DataTable tester = (DataTable)JsonConvert.DeserializeObject(newstring, (typeof(DataTable)));
+                    }
+                }
+
+            }
+        }
+
     }
 }
